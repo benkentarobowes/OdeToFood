b4357b072e6fd24d3fd669af721940efaa84779e
Ben Bowes <benkentarobowes@gmail.com>
2017-06-12 16:21:03 +0100

    Model AboutModel update

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,9 @@ namespace OdeToFood.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            var model = new AboutModel();
+            model.Name = "Ben Bowes";
+            model.Location = "Hammersmith, London, UK";
 
             return View();
         }
